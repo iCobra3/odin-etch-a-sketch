c812d54a79ba318df6eaacbdb12dfbaa79f11980
ahmad abdelaziz <ahmadabdelaziz8@gmail.com>
2024-11-24 21:24:18 +0200

    added button of gridsize and logic

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const container = document.querySelector('.container');
+let gridSizeButton = document.querySelector('.grid-button')
 
-for (let i = 0; i < 16 * 16; i++){
+function createGrid (size) {
+// create the 16 * 16 divs
+for (let i = 0; i < size * size; i++){
     const div = document.createElement('div');
     container.appendChild(div);
 }
+}
+
+gridSizeButton.addEventListener('click', () => {
+    let size = prompt('Enter the number of squares per side (max 100):');
+    size = Math.min(size, 100);
+    createGrid(size);
+})
